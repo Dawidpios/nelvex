f12714258844e2cb171662a1357f2f4b5d75daff
unknown <dawid.pios00@gmail.com>
2024-01-24 12:56:29 +0100

    Add zod to register form, refactor register form to new solution with zod

--- src/components/register/Register.tsx
+++ src/components/register/Register.tsx
@@ -1,115 +1,122 @@
-'use client'
-import { useRouter } from 'next/navigation'
+"use client";
+import { useRouter } from "next/navigation";
 import { useForm, SubmitHandler } from "react-hook-form";
-import toast, {Toaster} from 'react-hot-toast';
+import toast, { Toaster } from "react-hot-toast";
 import style from "./Register.module.scss";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 
-type RegisterValue = {
-  login: string;
-  password: string;
-  email: string;
-  date: string;
-  gender: string;
-  name: string;
-  surname: string;
-};
+const registerSchema = z.object({
+  login: z
+    .string()
+    .min(5, "User login is too short")
+    .max(20, "User login is too long"),
+  password: z.string().min(8, "User password is too short"),
+  email: z.string().email(),
+  name: z
+    .string()
+    .min(3, "User name is too short")
+    .max(20, "User name is too long"),
+  surname: z
+    .string()
+    .min(3, "User surname is too short")
+    .max(20, "User surname is too long"),
+});
+
+type typeRegisterSchema = z.infer<typeof registerSchema>;
 
 const Register = () => {
-  const router = useRouter()
+  const router = useRouter();
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm<RegisterValue>();
+  } = useForm<typeRegisterSchema>({
+    resolver: zodResolver(registerSchema),
+  });
 
-  const onSubmit: SubmitHandler<RegisterValue> = async (data) => {
-    fetch('/api/register', {
-      method: 'POST',
-      body: JSON.stringify(data)
-    }).then(async (res) => {
-      const {message} = await res.json()
-      if(res.status === 200) {
-        toast.success(message)
-        setTimeout(() => {
-          router.push('/')
-        }, 1500)
-      } else {
-        toast.error(message)
+  const onSubmit: SubmitHandler<typeRegisterSchema> = async (formData) => {
+    try {
+      const { success } = registerSchema.safeParse(formData);
+      if (!success) {
+        throw "Validation failed";
       }
-    }).catch(error => { throw new Error(error) })
+      fetch("/api/register", {
+        method: "POST",
+        body: JSON.stringify(formData),
+      }).then(async (res) => {
+        const { message } = await res.json();
+        if (res.status === 200) {
+          toast.success(message);
+          setTimeout(() => {
+            router.push("/");
+          }, 1500);
+        } else {
+          toast.error(message);
+        }
+      });
+    } catch (error) {
+      toast.error(error as string);
+    }
   };
 
   return (
     <>
-    <form className={style.form} onSubmit={handleSubmit(onSubmit)}>
-      <input
-        placeholder='Login'
-        className={style.form_textField}
-        {...register("login", {
-          required: { value: true, message: "User login is required" },
-          minLength: { value: 5, message: "User login is too short" },
-          maxLength: { value: 20, message: "User login is too long" },
-        })}
-      />
-      {errors.login && (
-        <span className={style.form_errorMessage}>{errors.login.message}</span>
-      )}
-      <input
-        placeholder='Name'
-        className={style.form_textField}
-        {...register("name", {
-          required: { value: true, message: "User name is required" },
-          minLength: { value: 3, message: "User name is too short" },
-          maxLength: { value: 20, message: "User name is too long" },
-        })}
-      />
-      {errors.name && (
-        <span className={style.form_errorMessage}>{errors.name.message}</span>
-      )}
-      <input
-        placeholder='Surname'
-        className={style.form_textField}
-        {...register("surname", {
-          required: { value: true, message: "User surname is required" },
-          minLength: { value: 3, message: "User surname is too short" },
-          maxLength: { value: 20, message: "User surname is too long" },
-        })}
-      />
-      {errors.surname && (
-        <span className={style.form_errorMessage}>
-          {errors.surname.message}
-        </span>
-      )}
-      <input
-        placeholder='E-mail'
-        className={style.form_textField}
-        type="email"
-        {...register("email", {
-          required: { value: true, message: "User email is required" },
-          minLength: { value: 5, message: "User email is too short" },
-        })}
-      />
-      {errors.email && (
-        <span className={style.form_errorMessage}>{errors.email.message}</span>
-      )}
-      <input
-        placeholder='Password'
-        className={style.form_textField}
-        {...register("password", {
-          required: { value: true, message: "User password is required" },
-          minLength: { value: 8, message: "User password is too short" },
-        })}
-      />
-      {errors.password && (
-        <span className={style.form_errorMessage}>
-          {errors.password.message}
-        </span>
-      )}
-      <button className={style.form_button} type="submit">
-        Register
-      </button>
-    </form>
-    <Toaster />
+      <form className={style.form} onSubmit={handleSubmit(onSubmit)}>
+        <input
+          placeholder="Login"
+          className={style.form_textField}
+          {...register("login")}
+        />
+        {errors.login && (
+          <span className={style.form_errorMessage}>
+            {errors.login.message}
+          </span>
+        )}
+        <input
+          placeholder="Name"
+          className={style.form_textField}
+          {...register("name")}
+        />
+        {errors.name && (
+          <span className={style.form_errorMessage}>{errors.name.message}</span>
+        )}
+        <input
+          placeholder="Surname"
+          className={style.form_textField}
+          {...register("surname")}
+        />
+        {errors.surname && (
+          <span className={style.form_errorMessage}>
+            {errors.surname.message}
+          </span>
+        )}
+        <input
+          placeholder="E-mail"
+          className={style.form_textField}
+          type="email"
+          {...register("email")}
+        />
+        {errors.email && (
+          <span className={style.form_errorMessage}>
+            {errors.email.message}
+          </span>
+        )}
+        <input
+          placeholder="Password"
+          className={style.form_textField}
+          {...register("password")}
+        />
+        {errors.password && (
+          <span className={style.form_errorMessage}>
+            {errors.password.message}
+          </span>
+        )}
+        <button className={style.form_button} type="submit">
+          Register
+        </button>
+      </form>
+      <Toaster />
     </>
   );
 };
