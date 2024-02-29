a3785de75062287a0659eb954cfc34d795827f0a
unknown <dawid.pios00@gmail.com>
2024-02-29 14:01:54 +0100

    Added styles to not found section

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,11 +1,12 @@
 import Link from 'next/link'
+import Button from '@/components/button/Button'
  
 export default function NotFound() {
   return (
-    <div>
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-      <Link href="/">Return Home</Link>
+    <div className='notFound'>
+      <h2 className='notFound_header'>404 Not Found</h2>
+      <p className='notFound_paragraph'>Your visited page not found. You may go home page.</p>
+      <Button className='notFoundButton'><Link href="/">Back to home page</Link></Button>
     </div>
   )
 }
