bdfe1fd50b3836f4247f34212ee7dacff61b0dc7
unknown <dawid.pios00@gmail.com>
2024-03-17 11:55:44 +0100

    Add Product component refactoring, added styling

--- app/product/add/AddProductForm.tsx
+++ app/product/add/AddProductForm.tsx
@@ -9,20 +9,33 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useState } from "react";
 import { Toaster, toast } from "react-hot-toast";
 import Button from "@/components/button/Button";
+import { revalidatePath } from "next/cache";
 
 const formSchema = z.object({
-  title: z.string().min(5, "Title must be 5 characters at least").max(15, "Maximum title length equals 15 characters"),
+  title: z
+    .string()
+    .min(5, "Title must be 5 characters at least")
+    .max(15, "Maximum title length equals 15 characters"),
   price: z.number().gt(0, "Price must be greater than 0"),
   stock: z.number().gt(0, "Quantity must be greater than 0"),
-  category: z.enum(["Jewelery", "Electronics", "Women's clothing", "Men's clothing", "Other"]),
-  description: z.string().min(10, "Description must be longer than 10 characters").max(200, "Description must be shorter than 200 characters"),
-  image: z.string().optional()
+  category: z.enum([
+    "Jewelery",
+    "Electronics",
+    "Women's clothing",
+    "Men's clothing",
+    "Other",
+  ]),
+  description: z
+    .string()
+    .min(10, "Description must be longer than 10 characters")
+    .max(200, "Description must be shorter than 200 characters"),
+  image: z.string().optional(),
 });
 
 type typeFormSchema = z.infer<typeof formSchema>;
 
 const AddProductForm = () => {
-  const [imageUrl, setImage] = useState<string | null>(null)
+  const [imageUrl, setImage] = useState<string | null>(null);
 
   const {
     register,
@@ -39,7 +52,7 @@ const AddProductForm = () => {
   ) => {
     
     try {
-      if(data.image) {
+      if(imageUrl) {
         data.image = imageUrl as string
       }
       const {success} = formSchema.safeParse({ ...data });
@@ -52,32 +65,44 @@ const AddProductForm = () => {
         cache: "no-cache",
       });
       action("getProducts");
+      // revalidatePath('/product')
       reset();
     } catch (error: any) {
-      toast.error(error)
+      toast.error(error);
     }
   };
   return (
     <>
-      <form className={styles.form} name="form" onSubmit={handleSubmit(onSubmit)}>
-        <label htmlFor="Title">Title</label>
-        <input {...register("title")} type="text" />
+      <form
+        className={styles.form}
+        name="form"
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <input placeholder="Product title" {...register("title")} type="text" />
         {errors.title && <p>{errors.title.message}</p>}
-        <label htmlFor="Price">Price</label>
-        <input {...register("price", { valueAsNumber: true })} />
+        <input
+          placeholder="Product price"
+          {...register("price", { valueAsNumber: true })}
+        />
         {errors.price && <p>{errors.price.message}</p>}
-        <label htmlFor="Stock">Quantity</label>
-        <input {...register("stock", { valueAsNumber: true })} type="number" />
+        <input
+          placeholder="Product quantity"
+          {...register("stock", { valueAsNumber: true })}
+          type="number"
+        />
         {errors.stock && <p>{errors.stock.message}</p>}
         <label htmlFor="Category">Category</label>
-        <select {...register('category')} >
-        <option value="Jewelery">Jewelery</option>
+        <select defaultValue={"Other"} {...register("category")}>
+          <option style={{color: "red", fontWeight:"bold", textAlign:"center"}} value="Jewelery">Jewelery</option>
+          <option style={{color: "red", fontWeight:"bold", textAlign:"center"}} value="Men's clothing">Men's clothing</option>
+          <option style={{color: "red", fontWeight:"bold", textAlign:"center"}} value="Women's clothing">Women's clothing</option>
+          <option style={{color: "red", fontWeight:"bold", textAlign:"center"}} value="Electronics">Electronics</option>
+          <option style={{color: "red", fontWeight:"bold", textAlign:"center"}} value="Other">Other</option>
         </select>
-        <AddImage setImage={setImage}/>
-        <label htmlFor="Description">Description</label>
-        <textarea {...register("description")} maxLength={200} />
+        <AddImage setImage={setImage} text={'Set product picture'} />
+        <textarea placeholder="Product description"{...register("description")} maxLength={200} />
         {errors.description && <p>{errors.description.message}</p>}
-        <Button type="submit">Wyślij</Button>
+        <Button className={"addProductButton"} type="submit">Wyślij</Button>
       </form>
       <Toaster />
     </>
