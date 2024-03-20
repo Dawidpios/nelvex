a5231084c34d0e8b745b14835c320b02db10cd02
unknown <dawid.pios00@gmail.com>
2024-03-20 15:08:01 +0100

    Added pagination component

--- app/product/page.tsx
+++ app/product/page.tsx
@@ -1,5 +1,8 @@
+import PaginationComponent from "@/components/pagination/PaginationComponent";
 import ProductsList from "@/components/products/ProductsList";
+import SearchBar from "@/components/searchBar/SearchBar";
 export const dynamic = 'force-dynamic'
+
 type Product = {
   id: number;
   title: string;
@@ -17,7 +20,7 @@ type Props = {
 }
 
 const getProducts = async () => {
-  const result = await fetch("http://localhost:3000/api/getProducts", {
+  const result = await fetch(process.env.URL_API +"/getProducts", {
     method: "GET",
     next: {
       tags: ['getProducts']
@@ -28,12 +31,21 @@ const getProducts = async () => {
 };
 
 
-const ProductList = async () => {
-  const products = await getProducts()
+const ProductList = async ({searchParams} : {searchParams: { [key: string]: string | string[] | undefined }}) => {
+  const productsResult = await getProducts()
 
+  const page = searchParams['page'] ?? '1'
+  const perPage = '5'
+
+  const paginStart = (Number(page) - 1) * (Number(perPage))
+  const end = paginStart + Number(perPage)
+
+  const products = productsResult.slice(paginStart, end)
+  
   return (
     <>
       <ProductsList products={products}></ProductsList>
+      <PaginationComponent dataLength={productsResult.length} ></PaginationComponent>
     </>
   );
 };
---
+++ src/components/pagination/PaginationComponent.tsx
@@ -0,0 +1,31 @@
+"use client";
+import React, {useState} from 'react'
+import Pagination from "@mui/material/Pagination";
+import { useRouter, useSearchParams } from "next/navigation";
+import styles from './PaginationComponent.module.scss'
+
+const PaginationComponent = ({
+  dataLength,
+}: {
+  dataLength: number;
+}) => {
+  const searchParams = useSearchParams()
+
+  const [page, setPage] = useState(Number(searchParams.get('page') || 1));
+  const router = useRouter();
+  const perPage = "5";
+  const length = dataLength;
+
+  const handleChange = (event:any, value:number) => {
+    router.push(`/product?page=${value}`);
+    setPage(value)
+  };
+
+  return (
+    <div className={styles.paginationComponent}>
+      <Pagination page={page} color="secondary" count={Math.ceil(length / Number(perPage))} onChange={handleChange}/>
+    </div>
+  );
+};
+
+export default PaginationComponent;
