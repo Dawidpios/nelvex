a4b3fa2f933d75c5abb8a90facec8e10e9abb549
unknown <dawid.pios00@gmail.com>
2024-03-25 13:18:36 +0100

    Added new fonts

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,14 +1,20 @@
 import Logo from "./Logo";
+import { Truculenta } from "next/font/google";
 import OfferArticle from "./offerArticle";
 import BestProducts from "./BestProducts";
 import styles from './homePage.module.scss'
 import { Suspense } from "react";
 export const dynamic = 'force-dynamic'
 
+const truculenta = Truculenta({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+});
+
 const HomePage = async () => {
 
   return (
-    <div>
+    <div className={truculenta.className}>
       <Logo />
       <main className={styles.main}>
         <OfferArticle />
